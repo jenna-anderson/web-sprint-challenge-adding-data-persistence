16e9800fe4acb8a7eb9efa3a870a8c97f0033042
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-16 10:18:04 -0600

    creates project_resources assignment table in first-migration

--- data/migrations/20210716155756_first-migration.js
+++ data/migrations/20210716155756_first-migration.js
@@ -24,10 +24,25 @@ exports.up = async function(knex) {
             .inTable('projects')
             .onDelete('CASCADE').onUpdate('CASCADE')
     })
+    .createTable('project_resources', table => {
+        table.increments('project_resource_id')
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('CASCADE').onUpdate('CASCADE')
+        table.integer('resource_id')
+            .unsigned()
+            .references('resource_id')
+            .inTable('resources')
+            .onDelete('CASCADE').onUpdate('CASCADE')
+    })
 };
 
 exports.down = async function(knex) {
   await knex.schema
+    .dropTableIfExists('project_resources')
     .dropTableIfExists('tasks')
     .dropTableIfExists('resources')
     .dropTableIfExists('projects')
