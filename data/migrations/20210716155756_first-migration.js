0e2561e98f1e25801aa2c531e182aefe2539208c
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-16 10:09:26 -0600

    creates resources table in first-migration

--- data/migrations/20210716155756_first-migration.js
+++ data/migrations/20210716155756_first-migration.js
@@ -7,9 +7,15 @@ exports.up = async function(knex) {
         table.string('project_description')
         table.boolean('project_completed')
     })
+    .createTable('resources', table => {
+        table.increments('resource_id')
+        table.string('resource_name').notNullable().unique()
+        table.string('resource_description')
+    })
 };
 
 exports.down = async function(knex) {
   await knex.schema
     .dropTableIfExists('projects')
+    .dropTableIfExists('resources')
 };
