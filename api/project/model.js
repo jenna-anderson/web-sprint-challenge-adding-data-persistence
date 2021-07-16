541028588919cc58eb8a01bbbf508b05e0b09d7c
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-16 11:25:38 -0600

    creates getProjects model function to return all projects with GET request

--- api/project/model.js
+++ api/project/model.js
@@ -1,6 +1,21 @@
+const db = require('../../data/dbConfig')
 
-const getProjects = () => {
-    console.log('getProjects successfully wired')
+const getProjects = async () => {
+    const projects = await db('projects')
+    const projectsWithBoolean = projects.map(project => {
+        if(project["project_completed"] === 0) {
+            return {
+                    ...project,
+                    "project_completed": false
+            }
+        } else {
+            return {
+                ...project,
+                "project_completed": true
+            }
+        }
+    })
+    return projectsWithBoolean
 }
 
 const createProject = () => {
--- api/project/router.js
+++ api/project/router.js
@@ -4,9 +4,13 @@ const router = express.Router()
 
 const Projects = require('./model')
 
-router.get('/', (req, res, next) => {
-    console.log('projectsRouter connected')
-    Projects.getProjects()
+router.get('/', async (req, res, next) => {
+    try{
+        const projects = await Projects.getProjects()
+        res.status(200).json(projects)
+    } catch(err) {
+        next(err)
+    }
 })
 
 router.post('/', (req, res, next) => {
