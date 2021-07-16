165a866fa0df4cac38d0b4275b8aeb5931bd5434
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-16 09:37:11 -0600

    wires routers to server

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,10 @@
-// build your `/api/projects` router here
+const express = require('express')
+const server = require('../server')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    console.log('projectsRouter connected')
+})
+
+module.exports = router
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,9 @@
-// build your `/api/tasks` router here
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    console.log('tasksRouter connected')
+})
+
+module.exports = router
