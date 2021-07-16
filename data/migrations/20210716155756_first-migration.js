6988c5df3b3b55e61ef2b89db3872aa3aa3746a6
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-07-16 10:07:30 -0600

    creates projects table in first-migration

---
+++ data/migrations/20210716155756_first-migration.js
@@ -0,0 +1,15 @@
+
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('projects', table => {
+        table.increments('project_id')
+        table.string('project_name').notNullable()
+        table.string('project_description')
+        table.boolean('project_completed')
+    })
+};
+
+exports.down = async function(knex) {
+  await knex.schema
+    .dropTableIfExists('projects')
+};
